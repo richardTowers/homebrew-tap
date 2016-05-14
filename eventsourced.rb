bb1cecc33f3590a10e00f56ca9ab4204efc3423f
Richard Towers <richard@richard-towers.com>
2016-05-14 17:51:51 +0100

    Add eventsourced

---
+++ eventsourced.rb
@@ -0,0 +1,9 @@
+class Eventsourced < Formula
+  homepage "https://github.com/richardTowers/eventsourced"
+  url "https://github.com/richardTowers/eventsourced/releases/download/1.0.0/eventsourced"
+  sha1 "a54d76f0cb7a016f0b6abeb7ef19739496fe42b9"
+
+  def install
+    bin.install "eventsourced"
+  end
+end
